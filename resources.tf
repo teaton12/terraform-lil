ee30efa32b6b2ab0eaf8b8f183cf92a1e263eacf
teaton12 <tomeaton12@hotmail.com>
2019-07-15 20:40:39 +0100

    Working resources file

--- resources.tf
+++ resources.tf
@@ -3,8 +3,7 @@ resource "aws_vpc" "environment-example-two" {
 	enable_dns_hostnames = true
 	enable_dns_support = true
 
-	tags {
-	Name = "terraform-aws-vpc-example-two"
-	Region = "eu-west-2"
-	}
+  tags = {
+    Name = "terraform-aws-vpc-example-two"
+  }
 }
