04fafe69041d28c3dd2837f29f78134f84362c0f
teaton12 <tomeaton12@hotmail.com>
2019-08-12 16:29:20 +0100

    n/a

--- resources.tf
+++ resources.tf
@@ -13,3 +13,23 @@ resource "aws_subnet" "subnet1" {
 	vpc_id = "${aws_vpc.environment-example-two.id}"
 	availability_zone = "eu-west-2a"
 }
+
+resource "aws_subnet" "subnet2" {
+	cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 2, 2)}"
+	vpc_id = "${aws_vpc.environment-example-two.id}"
+	availability_zone = "eu-west-2b"
+}
+
+resource "aws_security_group" "subnetsecurity" {
+	vpc_id = "${aws_vpc.environment-example-two.id}"
+
+	ingress {
+		cidr_blocks = [
+			"${aws_vpc.environment-example-two.cidr_block}"
+		]
+
+		from_port = 80
+		to_port = 80
+		protocol = "tcp"
+	}
+}
